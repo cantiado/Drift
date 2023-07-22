62f48342f1dcbe66d8ecf9ce5850c7457d29433b
fchau1 <fchau@nevada.unr.edu>
2023-07-22 16:04:11 -0700

    set up routes and screens

---
+++ Drift/assets/theme.js
@@ -0,0 +1,42 @@
+const theme = {
+    dark: false,
+    roundness: 4,
+    colors: {
+      //dark blue
+      primary: '#5383F5',
+      //light blue
+      accent: '#5AD6C5',
+      //green
+      background: '#72EB6C',
+      //gold
+      surface: '#D6D276',
+      text: '#333333',
+      disabled: 'rgba(0, 0, 0, 0.26)',
+      placeholder: 'rgba(0, 0, 0, 0.54)',
+      backdrop: 'rgba(0, 0, 0, 0.5)',
+      //yellow
+      onSurface: '#FAC56A',
+      notification: '#FF0000',
+    },
+    fonts: {
+      regular: {
+        fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif',
+        fontWeight: 'normal',
+      },
+      medium: {
+        fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif',
+        fontWeight: '500',
+      },
+      light: {
+        fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif',
+        fontWeight: '300',
+      },
+      thin: {
+        fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif',
+        fontWeight: '100',
+      },
+    },
+  };
+  
+  export default theme;
+  
---
+++ Drift/screens/MessagesScreen.js
@@ -0,0 +1,13 @@
+import { Appbar } from "react-native-paper";
+
+const MessagesScreen = () => {
+  return (
+    <View>
+      <Appbar.Header>
+        <Appbar.Content title="Messages" />
+      </Appbar.Header>
+    </View>
+  );
+};
+
+export default MessagesScreen;
