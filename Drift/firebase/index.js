9baa1aec015d4b814f96987dbd1861b3fe767699
Carl Antiado <carl.antiado01@gmail.com>
2023-07-30 21:04:06 -0500

    Installed firebase and created initial files

---
+++ Drift/firebase/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJDGui6yVyY2E4eGF3sPuLqoAixXPtUvk",
+  authDomain: "drift-6498e.firebaseapp.com",
+  projectId: "drift-6498e",
+  storageBucket: "drift-6498e.appspot.com",
+  messagingSenderId: "206449071616",
+  appId: "1:206449071616:web:2efd820aabc5d408f9eae5",
+  measurementId: "G-749QDYLZCE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
