9c345e8f25c86bc8595ac4d13e719d8249b9b711
Diane Du <dianedu@nevada.unr.edu>
2023-08-28 21:59:39 -0700

    attempted to style card, but am getting errors

--- Drift/screens/CartScreen.js
+++ Drift/screens/CartScreen.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { View, FlatList, Image, Text, Button } from "react-native";
-import { IconButton, Card, Title } from "react-native-paper";
+import { View, FlatList, Image, Text } from "react-native";
+import { IconButton, Card, Title, MD3Colors } from "react-native-paper";
 import { getManyItemData, getUserData } from "../firebase/database";
 import { getCurrentUserUID } from "../firebase/authentication";
 
@@ -31,16 +31,39 @@ const cartItems = [
 
 const CartScreen = ({ navigation }) => {
   const renderItems = ({ item }) => (
-    <Card style={{ margin: 1, padding: 0 }} elevation={0}>
-      <Card.Content style={{ padding: 0 }}>
+    <Card style={{ margin: 1, padding: 0 }} mode="outlined" elevation={0}>
+      <Card.Content style={{ flexDirection: 'row', justifyContent: 'flex-start' }} >
         <Image
           source={{
             uri: item.images[0],
           }}
-          style={{ width: "100%", height: 100 }}
+          style={{ width: "30%", height: 100 }}
           resizeMode="cover"
+          //style={{ padding: 0 }}
         />
-        <Text>{`Size: ${item.size} Price ${item.price}`}</Text>
+        {/* <Text>{`Size: ${item.size} Price: $${item.price}`}</Text> */}
+        {/* <Card mode="outlined" elevation={0} style={{width: '70%'}}>
+          <Card.Content style={{flexDirection: 'column', justifyContent:'flex-start', width: "100%", paddingLeft: "5px",  paddingRight: "5px"}}>
+          <Text style={{overflow: 'hidden'}} numberOfLines={1} ellipsizeMode='tail'>{`${item.name}`}</Text>
+            <Text>{`Size: ${item.size}`}</Text>
+            <Text>{`Price: $${item.price}`}</Text>
+            
+          </Card.Content>
+          <IconButton 
+              style={{position:'relative'}}
+              icon="camera"
+              iconColor={MD3Colors.error50}
+              size={20}
+              onPress={() => console.log('Pressed')}
+            />
+        </Card> */}
+        <Card mode="outlined" elevation={0} style={{width: '70%'}}>
+          <Card.Title 
+            title={`${item.name}`}
+            subtitle={`Size: ${item.size} Price: $${item.price}`}
+            right={() => <IconButton icon="trash" onPress={() => {}} />}
+          />
+        </Card>
       </Card.Content>
     </Card>
   );
