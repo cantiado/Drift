55ac287576111f7dcff0bc57d443e37f5cd5708f
fchau1 <fchau@nevada.unr.edu>
2023-08-29 21:11:21 -0700

    Update sellerProfile

--- Drift/screens/SellerProfileScreen.js
+++ Drift/screens/SellerProfileScreen.js
@@ -3,9 +3,18 @@ import * as React from "react";
 import { View } from "react-native";
 
 
-const SellerProfileScreen = ({ navigation, owner }) => {
+const SellerProfileScreen = ({ navigation, sellerID }) => {
 //fetchSellersItems
 //fetchOwnerName
+const [owner, setOwner] = React.useState("");
+  getUserData(sellerID).then((res) =>
+    setOwner(res.first + " " + res.last)
+  );
+
+const [items, setItems] = React.useState([]);
+getUserData(sellerID)
+    .then((user) => getManyItemData(user.items, -1))
+    .then((items) => setItems(items));
   return (
     <View>
 
