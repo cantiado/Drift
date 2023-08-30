e26e8b439a71dccf628b7bf2687e64179d12f10e
fchau1 <fchau@nevada.unr.edu>
2023-08-29 21:08:01 -0700

    ui changes

---
+++ Drift/screens/SellerProfileScreen.js
@@ -0,0 +1,16 @@
+import { Appbar } from "react-native-paper";
+import * as React from "react";
+import { View } from "react-native";
+
+
+const SellerProfileScreen = ({ navigation, owner }) => {
+//fetchSellersItems
+//fetchOwnerName
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default SellerProfileScreen;
